d4ab2cfc1d751f472c85160f2e7a80c123af7004
AdaCAD <adacad@AdaCADs-MacBook-Air.local>
2021-11-18 00:57:20 -0700

    change so links can't use yellow

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
 	$('body').removeClass("loading");
 	
-	var clss = ["color-green", "color-blue", "color-red", "color-yellow"];
+	var clss = ["color-green", "color-red", "color-blue", "color-yellow"];
 	$('strong').each(function() {
 		var i = Math.floor(Math.random() * 4); 
 		this.classList.add(clss[i]);
 	});
 
 	$('.reg-ref').each(function() {
-		var i = Math.floor(Math.random() * 4); 
+		var i = Math.floor(Math.random() * 3); 
 		this.classList.add(clss[i]);
 	});
 
