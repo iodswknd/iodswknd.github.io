a1a69a158c289b4604f7b723eeb859c45088a0a1
AdaCAD <adacad@AdaCADs-MacBook-Air.local>
2021-11-23 20:18:15 -0700

    make favicon rotate between the iods icons

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 $(document).ready(function(){
+	var icons = ["/images/tintedcolor_sugawara.png", "/images/tintedcolor_oikawa.png", "/images/tintedcolor_daichi.png", "/images/tintedcolor_iwaizumi.png"]
+	var i = Math.floor(Math.random() * 4);
+
+	$('#icon')[0].href = icons[i];
+
+
+
 	$('body').removeClass("loading");
 	
 	var clss = ["color-green", "color-red", "color-blue", "color-yellow"];
 	$('strong').each(function() {
-		var i = Math.floor(Math.random() * 4); 
+		i = Math.floor(Math.random() * 4); 
 		this.classList.add(clss[i]);
 	});
 
 	$('.reg-ref').each(function() {
-		var i = Math.floor(Math.random() * 2); 
+		i = Math.floor(Math.random() * 2); 
 		this.classList.add(clss[i]);
 	});
 
